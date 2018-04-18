54aeadb82758a32c5d20051799647fb56e676594
Dana <danaciocoiu78@gmail.com>
2018-04-18 14:53:50 +0300

    Customer

---
+++ Curs 3/Curs 3/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : ApiController
+    {  //poarta de intrare in web service, o metoda care ret toata lista de clienti
+        public List<Customer> GetCustomers()
+        {
+            using (CURS3Entities1 container = new CURS3Entities1())
+            {
+                return container.Customers.ToList();
+            }
+        }
+
+    }
+}
---
+++ Curs 3/Curs 3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Curs_3.WebApiApplication" Language="C#" %>
